271dd838075331044f3623924c17f9916e57c74c
virenribadiya <vribadiya278@rku.ac.in>
2023-01-02 23:53:06 +0530

    use Axios to perform Network Requests

---
+++ QuestionTime/frontend/src/common/api.service.js
@@ -0,0 +1,6 @@
+const axios = require("axios");
+
+axios.defaults.xsrfCookieName = "csrftoken"; // generating token value
+axios.defaults.xsrfHeadername = "X-CSRFTOKEN"; // adding it as a header for each request
+
+export {axios};
--- QuestionTime/frontend/src/views/Home.vue
+++ QuestionTime/frontend/src/views/Home.vue
@@ -1,14 +1,43 @@
 <template>
-  <div>
+  <div class="home">
     <h1>Home page!!</h1>
+    <button v-on:click="getQuestions">Load questions</button>
+    <div v-if="questions">
+      <li v-for="question in questions" v-bind:key="question.id">{{question}}</li>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import {axios} from "@/common/api.service.js";
+
 export default {
   name: 'HomeView',
+
+  data()
+  {
+    return{
+      questions:[]
+    }
+  },
+  methods:
+  {
+    async getQuestions(){
+      let endpoint = "/api/v1/questions/";
+      try {
+        const response = await axios.get(endpoint);
+        //console.log(response);
+        this.questions = response.data;
+        console.log(this.questions);
+         
+      } catch (error) {
+        console.log(error.response);
+        alert(error.response.statusText);
+      }
+    }
+  }
   
 }
 </script>
