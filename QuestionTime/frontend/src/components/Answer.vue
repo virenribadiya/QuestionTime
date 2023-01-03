fe1ce8f0abeb54d5500f48d7db6a74b4df3dd7bc
virenribadiya <vribadiya278@rku.ac.in>
2023-01-03 16:09:46 +0530

    The Answer Component

---
+++ QuestionTime/frontend/src/components/Answer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="card shadow p-2 mb-4 bg-body rounded">
+        <p class="text-muted">
+            <strong>{{answer.author}} &#8901; {{answer.created_at}}</strong>
+        </p>
+        <p style="white-space: pre-wrap;">{{answer.body}}</p>
+    </div>
+
+</template>
+
+
+<script>
+
+
+export default{
+    name:"AnsewerComponent",
+    props: {
+        answer:{
+            type:Object,
+            required:true
+        }
+    }
+}
+</script>
+
+
+<style>
+.shadow-color{
+    box-shadow: lightgreen;
+}
+</style>
--- QuestionTime/frontend/src/views/Question.vue
+++ QuestionTime/frontend/src/views/Question.vue
@@ -1,47 +1,66 @@
 <template>
     <div class="container mt-3">
         <div v-if="question">
-            <h1>{{question.content}}</h1>
+            <h1>{{ question.content }}</h1>
             <p class="mb-0 ">
-              Posted by :
-              <span class="question-author">{{  question.author  }}</span>
+                Posted by :
+                <span class="question-author">{{ question.author }}</span>
             </p>
             <p>
-                {{question.created_at}}
+                {{ question.created_at }}
             </p>
         </div>
         <div v-else class="error text-center h3">
             404 - Question Not Found
         </div>
+        <hr>
+
+        <div v-if="question" class="">
+            <AnsewerComponent v-for="answer in answers" :key="answer.uuid" :answer="answer" />
+        </div>
+        <!-- {{answers}} -->
+        <div class="my-4">
+            <p v-show="loadingAnswers">....Loading....</p>
+            <button v-show="next" @click="getQuestionAnswers" class="btn btn-sm btn-outline-success">
+                Load More
+            </button>
+        </div>
     </div>
+
 </template>
 
 
 <script>
 
 import { axios } from "@/common/api.service.js";
+import AnsewerComponent from "@/components/Answer.vue";
 
-export default{
-    name:"QuesTion",
+export default {
+    name: "QuesTion",
     props:
     {
-        slug:{
-            type:String,
-            required:true,
+        slug: {
+            type: String,
+            required: true,
         }
     },
-    data()
-    {
-        return{
+    components: {
+        AnsewerComponent
+    },
+    data() {
+        return {
             question: null,
+            answers: [],
+            next: null,
+            loadingAnswers: false,
         }
     },
     methods:
     {
-        setPageTitle(title){
+        setPageTitle(title) {
             document.title = title;
         },
-        async getQuestionData(){
+        async getQuestionData() {
             const endpoint = `/api/v1/questions/${this.slug}/`;
             try {
                 const response = await axios.get(endpoint);
@@ -51,13 +70,38 @@ export default{
                 console.log(error.response);
                 const title = "404 - Not Found!";
                 this.setPageTitle(title);
-                alert(error.response.statusText);     
+                alert(error.response.statusText);
+            }
+        },
+        async getQuestionAnswers() {
+            let endpoint = `/api/v1/questions/${this.slug}/answers/`;
+            if (this.next) {
+                endpoint = this.next;
+            }
+            this.loadingAnswers = true;
+            try {
+                const response = await axios.get(endpoint);
+                console.log(response);
+                this.answers.push(...response.data.results);
+                //console.log(this.answers);
+                this.loadingAnswers = false;
+                if (response.data.next) {
+                    this.next = response.data.next;
+                }
+                else {
+                    this.next = null;
+                }
+                console.log(this.questions);
+
+            } catch (error) {
+                console.log(error.response);
+                alert(error.response.statusText);
             }
         }
     },
-    created()
-    {
+    created() {
         this.getQuestionData();
+        this.getQuestionAnswers();
     }
 }
 </script>
@@ -65,11 +109,12 @@ export default{
 
 
 <style>
-.question-author{
+.question-author {
     font-weight: bold;
     color: #dc3545;
 }
-.error{
+
+.error {
     color: #dc3545;
 }
 </style>
