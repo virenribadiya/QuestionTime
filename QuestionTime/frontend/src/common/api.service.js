8b41bb26a6fa14c581ec1dc1bee02926dbe99059
virenribadiya <vribadiya278@rku.ac.in>
2023-01-03 22:38:51 +0530

    #Solved Axios config error

--- QuestionTime/frontend/src/common/api.service.js
+++ QuestionTime/frontend/src/common/api.service.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
 axios.defaults.xsrfCookieName = "csrftoken"; // generating token value
-axios.defaults.xsrfHeadername = "X-CSRFTOKEN"; // adding it as a header for each request
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN"; // adding it as a header for each request
 
 export {axios};
